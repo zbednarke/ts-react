053d4dfd3062c3be596e4761b7dbaf5446234fba
zbednarke <zbednarke@gmail.com>
2023-06-16 16:13:24 -0700

    init

---
+++ simple-typescript/src/index.js
@@ -0,0 +1,6 @@
+function multiply(a, b) {
+    return a * b;
+}
+var a = 4;
+var b = 2;
+console.log(multiply(a, b));
---
+++ simple-typescript/src/index.ts
@@ -0,0 +1,8 @@
+function multiply(a: number, b: number): number {
+    return a * b;
+}
+
+const a = 2;
+const b = 3;
+
+console.log(multiply(a, b));
