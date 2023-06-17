8e963e7a2aa0eddd25aab08457eace64b974e206
zbednarke <zbednarke@gmail.com>
2023-06-17 15:43:24 -0700

    ts-loader configured with webpack

--- react-plus-typescript/webpack.config.js
+++ react-plus-typescript/webpack.config.js
@@ -1,5 +1,16 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
+    entry: "./src/index.ts",
+    resolve: {
+        extensions: [".js", '.ts', '.tsx'],
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/,
+        }]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
